6081ae611a534faf34fcd1ad1ca66e641570adae
Randell Janus <randelljanus@Randells-MacBook-Air.local>
2022-08-10 13:35:59 +0800

    add search feature

--- src/components/Layouts.js
+++ src/components/Layouts.js
@@ -1,12 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export const PageLayout = ({ children }) => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleTextChange = (e) => {
+    const target = e.target.value.replace(/\s+/g, "");
+    setSearchTerm(target.toLowerCase());
+  };
+
+  const handleSearch = () => {
+    navigate(`/pokemon/${searchTerm.toLocaleLowerCase()}`);
+  };
+
   return (
     <div className="py-24 min-h-screen max-w-7xl mx-auto px-8">
-      <h1 className="font-semibold border-b pb-8 mb-8 w-full">
-        <Link to="/">Pokedex</Link>
-      </h1>
+      <div className="font-bold border-b pb-8 mb-8 flex items-center justify-between">
+        <h1>
+          <Link to="/">Pokedex</Link>
+        </h1>
+        <form onSubmit={handleSearch} className="space-x-4">
+          <input
+            type="text"
+            required
+            className="input-primary-slate"
+            onChange={handleTextChange}
+          />
+          <button className="btn-primary-slate">search</button>
+        </form>
+      </div>
       {children}
     </div>
   );
